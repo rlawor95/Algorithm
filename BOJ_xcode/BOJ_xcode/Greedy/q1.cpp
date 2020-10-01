236812a71369f00608f9c5d23e8c104fee32a7c4
KimJack <kjk71978292@gmail.com>
2020-10-01 18:04:39 +0900

    모험가 길드 문제풀이

---
+++ BOJ_xcode/BOJ_xcode/Greedy/q1.cpp
@@ -0,0 +1,40 @@
+//
+//  q1.cpp
+//  BOJ_xcode
+//
+//  Created by 김재경 on 2020/10/01.
+//  Copyright © 2020 김재경. All rights reserved.
+//  모험가 길드 with 나동빈
+
+
+#include <stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+#define MAX 100001
+
+int arr[MAX];
+
+int main()
+{
+    int n,result =0;
+    cin>>n;
+    
+    for(int i=0; i<n; i++)
+    {
+        int p;
+        cin>>p;
+        arr[p]++;
+    }
+    
+    for(int i=1; i<MAX; i++)
+    {
+        result += arr[i]/i;
+    }
+    
+    cout<<result<<endl;
+    
+    return 0;
+}
